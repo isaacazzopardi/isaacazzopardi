0120f83537fd39d91b9c62fe90911aadd9b9436c
pepren <76995750+pepren@users.noreply.github.com>
2022-07-13 20:49:36 +0100

    Delete writingportfolio.js

--- writingportfolio.js
+++
@@ -1,7 +0,0 @@
-var scrollWrapper = document.querySelector(".scrolling-wrapper");
-
-scrollWrapper.addEventListener('wheel', function(e) {
-
-  if (e.deltaY > 0) scrollWrapper.scrollLeft += 20;
-  else scrollWrapper.scrollLeft -= 20;
-});
